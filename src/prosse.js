ae63c904e5a9390ca34cf74318cebb9e73c5757f
CoDaTi <kevin@utocat.com>
2021-11-19 04:09:20 +0000

    feat: revolution MKR!!

--- src/prosse.js
+++ src/prosse.js
@@ -6,17 +6,15 @@ const processEvent = require("./service/processEvent");
 const { getNextPixel } = require("./service/image");
 const { gestionRole } = require("./service/gestion");
 
-
-
 const apiURL = process.env.URL_API;
 processEvent.on("config", async (config) => {
   let image;
   let pixelNeedChange;
 
   let messageOfPixel;
-  const myDepartement = (await axios.get(
-    `${apiURL}/departements/?name=${config.departementName}`
-  )).data
+  const myDepartement = (
+    await axios.get(`${apiURL}/departements/?name=${config.departementName}`)
+  ).data;
   try {
     console.log(config);
     const client = await discord.client;
@@ -44,8 +42,19 @@ processEvent.on("config", async (config) => {
         apiURL,
         myDepartement
       );
-      if(!config.surveil){
+      if (!config.surveil) {
         await channel_image.send(message);
+      }
+      if(config.revolution){
+        if (pixelNeedChange.length) {
+          process.send({
+            type: "alert",
+            data: {
+              pixelNeedChange,
+              prefix:"MKR revolution "
+            },
+          });
+        }
       }
       messageOfPixel = await discord.sendMessageForPixelChange(
         pixelNeedChange,
@@ -55,37 +64,44 @@ processEvent.on("config", async (config) => {
         ""
       );
     }
-    let allrole = Object.values(config.role.region);
-    allrole.push(config.role.valide);
-
+    let allrole;
+    if(!config.revolution)
+    {
+      allrole = Object.values(config.role.region);
+      allrole.push(config.role.valide);
+    }
     console.log(`Logged in as ${client.user.tag} in ${guild.name}!`);
 
     channel_log.send("I am up!");
-    if(config.surveil){
-      processEvent.on("react",async(pixel)=>{
-        console.log("react",pixel.x,pixel.y)
-        if(messageOfPixel[pixel.x] && messageOfPixel[pixel.x][pixel.y]){
-          if(!(await messageOfPixel[pixel.x][pixel.y].message).deleted){
-            await (await messageOfPixel[pixel.x][pixel.y].message).react(config.reaction.positif);
+    if (config.surveil) {
+      processEvent.on("react", async (pixel) => {
+        console.log("react", pixel.x, pixel.y);
+        if (messageOfPixel[pixel.x] && messageOfPixel[pixel.x][pixel.y]) {
+          if (!(await messageOfPixel[pixel.x][pixel.y].message).deleted) {
+            await (
+              await messageOfPixel[pixel.x][pixel.y].message
+            ).react(config.reaction.positif);
           }
         }
       });
-      processEvent.on("unreact",async(pixel)=>{
-        console.log("unreact",pixel.x,pixel.y)
-        if(messageOfPixel[pixel.x] && messageOfPixel[pixel.x][pixel.y]){
-          if(!(await messageOfPixel[pixel.x][pixel.y].message).deleted){
-            await (await messageOfPixel[pixel.x][pixel.y].message).reactions.removeAll();
+      processEvent.on("unreact", async (pixel) => {
+        console.log("unreact", pixel.x, pixel.y);
+        if (messageOfPixel[pixel.x] && messageOfPixel[pixel.x][pixel.y]) {
+          if (!(await messageOfPixel[pixel.x][pixel.y].message).deleted) {
+            await (
+              await messageOfPixel[pixel.x][pixel.y].message
+            ).reactions.removeAll();
           }
         }
       });
-      processEvent.on("alert",async(data)=>{
+      processEvent.on("alert", async (data) => {
         console.log(data);
         messageOfPixel = await discord.sendMessageForPixelChange(
           data.pixelNeedChange,
           channel_image,
           messageOfPixel,
           config,
-          `<@&${config.role.gardien}> `
+          `${data.prefix?data.prefix:"<@&${config.role.gardien}> "}`
         );
       });
     }
@@ -106,9 +122,12 @@ processEvent.on("config", async (config) => {
           myDepartement
         );
         if (pixelNeedChange.length) {
-          process.send({type:"alert",data:{
-            pixelNeedChange,
-          }});
+          process.send({
+            type: "alert",
+            data: {
+              pixelNeedChange,
+            },
+          });
           messageOfPixel = await discord.sendMessageForPixelChange(
             pixelNeedChange,
             channel_image,
@@ -124,9 +143,9 @@ processEvent.on("config", async (config) => {
         messageOfPixel[pixel.x][pixel.y]
       ) {
         if (messageOfPixel[pixel.x][pixel.y].pixel.color === pixel.hexColor) {
-          if(!(await messageOfPixel[pixel.x][pixel.y].message).deleted){
+          if (!(await messageOfPixel[pixel.x][pixel.y].message).deleted) {
             await (await messageOfPixel[pixel.x][pixel.y].message).delete();
-            messageOfPixel[pixel.x][pixel.y]= undefined;
+            messageOfPixel[pixel.x][pixel.y] = undefined;
             let messages = await channel_image.messages.fetch();
             if (messages.size === 1 && !config.surveil) {
               await messages.last().delete();
@@ -136,6 +155,19 @@ processEvent.on("config", async (config) => {
                 apiURL,
                 myDepartement
               );
+              if(config.revolution){
+                if(!config.revolution){
+                  if (pixelNeedChange.length) {
+                    process.send({
+                      type: "alert",
+                      data: {
+                        pixelNeedChange,
+                        prefix:"MKR revolution "
+                       },
+                    });
+                  }
+                }
+              }
               await channel_image.send(message);
               messageOfPixel = await discord.sendMessageForPixelChange(
                 pixelNeedChange,
@@ -148,158 +180,188 @@ processEvent.on("config", async (config) => {
           }
         }
       }
-
-      if (pixel.modifier.author !== pixel.author) {
-        if (
-          pixel.oldHexColor ===
-          (image &&
-          image[pixel.x - myDepartement.min.x] &&
-          image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y] &&
-          image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y] !== 0
-            ? image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y]
-            : config.color)
-        ) {
-          const member = await discord.getMemberByCoordonne(
-            config.idGuild,
-            pixel.x,
-            pixel.y
-          );
-          if (member && channel) {
-            await (
-              await channel.send(
-                `${
-                  member.nickname ? member.nickname : member.user.username
-                }! ton pixel a été changé de couleur!`
-              )
-            ).react(config.reaction.negatif);
-          }
-        } else if (
-          pixel.hexColor ===
-          (image &&
-          image[pixel.x - myDepartement.min.x] &&
-          image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y] &&
-          image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y] !== 0
-            ? image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y]
-            : config.color)
-        ) {
-          const member = await discord.getMemberByCoordonne(
-            config.idGuild,
-            pixel.x,
-            pixel.y
-          );
-          if (channel) {
-            await (
-              await channel.send(
-                `${
-                  member
-                    ? member.nickname
+      if(!config.revolution)
+      {
+        if (pixel.modifier.author !== pixel.author) {
+          if (
+            pixel.oldHexColor ===
+            (image &&
+            image[pixel.x - myDepartement.min.x] &&
+            image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y] &&
+            image[pixel.x - myDepartement.min.x][
+              pixel.y - myDepartement.min.y
+            ] !== 0
+              ? image[pixel.x - myDepartement.min.x][
+                  pixel.y - myDepartement.min.y
+                ]
+              : config.color)
+          ) {
+            const member = await discord.getMemberByCoordonne(
+              config.idGuild,
+              pixel.x,
+              pixel.y
+            );
+            if (member && channel) {
+              await (
+                await channel.send(
+                  `${
+                    member.nickname ? member.nickname : member.user.username
+                  }! ton pixel a été changé de couleur!`
+                )
+              ).react(config.reaction.negatif);
+            }
+          } else if (
+            pixel.hexColor ===
+            (image &&
+            image[pixel.x - myDepartement.min.x] &&
+            image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y] &&
+            image[pixel.x - myDepartement.min.x][
+              pixel.y - myDepartement.min.y
+            ] !== 0
+              ? image[pixel.x - myDepartement.min.x][
+                  pixel.y - myDepartement.min.y
+                ]
+              : config.color)
+          ) {
+            const member = await discord.getMemberByCoordonne(
+              config.idGuild,
+              pixel.x,
+              pixel.y
+            );
+            if (channel) {
+              await (
+                await channel.send(
+                  `${
+                    member
                       ? member.nickname
-                      : member.user.username
-                    : `[${pixel.x}:${pixel.y}]`
-                } arbore nos couleurs de force!`
-              )
-            ).react(config.reaction.positif);
+                        ? member.nickname
+                        : member.user.username
+                      : `[${pixel.x}:${pixel.y}]`
+                  } arbore nos couleurs de force!`
+                )
+              ).react(config.reaction.positif);
+            }
           }
-        }
-        return;
-      }
-
-      if (departement && config.regionName === departement.region) {
-        const member = await discord.getMemberByCoordonne(
-          config.idGuild,
-          pixel.x,
-          pixel.y
-        );
-        if (
-          pixel.hexColor ===
-          (image &&
-          image[pixel.x - myDepartement.min.x] &&
-          image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y] &&
-          image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y] !== 0
-            ? image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y]
-            : config.color)
-        ) {
-          if (channel) {
-            await (
-              await channel.send(
-                `${
-                  member
-                    ? member.nickname
-                      ? member.nickname
-                      : member.user.username
-                    : `[${pixel.x}:${pixel.y}]`
-                } arbore nos couleurs !`
-              )
-            ).react(config.reaction.positif);
-          }
-          await discord.addRole(
-            member,
-            config.role.valide,
-            allrole,
-            channel_log
-          );
           return;
+        }
+
+        if (departement && config.regionName === departement.region) {
+          const member = await discord.getMemberByCoordonne(
+            config.idGuild,
+            pixel.x,
+            pixel.y
+          );
+          if (
+            pixel.hexColor ===
+            (image &&
+            image[pixel.x - myDepartement.min.x] &&
+            image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y] &&
+            image[pixel.x - myDepartement.min.x][
+              pixel.y - myDepartement.min.y
+            ] !== 0
+              ? image[pixel.x - myDepartement.min.x][
+                  pixel.y - myDepartement.min.y
+                ]
+              : config.color)
+          ) {
+            if (channel) {
+              await (
+                await channel.send(
+                  `${
+                    member
+                      ? member.nickname
+                        ? member.nickname
+                        : member.user.username
+                      : `[${pixel.x}:${pixel.y}]`
+                  } arbore nos couleurs !`
+                )
+              ).react(config.reaction.positif);
+            }
+            await discord.addRole(
+              member,
+              config.role.valide,
+              allrole,
+              channel_log
+            );
+            return;
+          } else if (
+            pixel.hexColor.toUpperCase() ===
+            (image &&
+            image[pixel.x - myDepartement.min.x] &&
+            image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y] &&
+            image[pixel.x - myDepartement.min.x][
+              pixel.y - myDepartement.min.y
+            ] !== 0
+              ? image[pixel.x - myDepartement.min.x][
+                  pixel.y - myDepartement.min.y
+                ]
+              : config.color)
+          ) {
+            await discord.addRole(
+              member,
+              config.role.region[config.regionName],
+              allrole,
+              channel_log
+            );
+            if (channel) {
+              await (
+                await channel.send(
+                  `${
+                    member ? `<@${member.id}>` : `[${pixel.x}:${pixel.y}]`
+                  } nous trompe pour le ${pixel.hexColor} !`
+                )
+              ).react(config.reaction.negatif);
+            }
+            return;
+          } else {
+            await discord.addRole(
+              member,
+              config.role.region[config.regionName],
+              allrole,
+              channel_log
+            );
+            return;
+          }
         } else if (
           pixel.hexColor.toUpperCase() ===
           (image &&
           image[pixel.x - myDepartement.min.x] &&
           image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y] &&
-          image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y] !== 0
+          image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y] !==
+            0
             ? image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y]
             : config.color)
         ) {
-          await discord.addRole(
-            member,
-            config.role.region[config.regionName],
-            allrole,
-            channel_log
-          );
           if (channel) {
-            await (
-              await channel.send(
-                `${
-                  member ? `<@${member.id}>` : `[${pixel.x}:${pixel.y}]`
-                } nous trompe pour le ${pixel.hexColor} !`
-              )
-            ).react(config.reaction.negatif);
-          }
-          return;
-        } else {
-          await discord.addRole(
-            member,
-            config.role.region[config.regionName],
-            allrole,
-            channel_log
-          );
-          return;
-        }
-      } else if (
-        pixel.hexColor.toUpperCase() ===
-        (image &&
-        image[pixel.x - myDepartement.min.x] &&
-        image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y] &&
-        image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y] !== 0
-          ? image[pixel.x - myDepartement.min.x][pixel.y - myDepartement.min.y]
-          : config.color)
-      ) {
-        if (channel) {
-          if (departement) {
-            await (
-              await channel.send(
-                `[${pixel.x}:${pixel.y}] de la région "${departement.region}" ("${departement.name}") arbore notre couleur !`
-              )
-            ).react(config.reaction.positif);
-          } else {
-            await (
-              await channel.send(
-                `[${pixel.x}:${pixel.y}] d'un territoire inconnu a adopté la bonne couleur !`
-              )
-            ).react(config.reaction.positif);
+            if (departement) {
+              await (
+                await channel.send(
+                  `[${pixel.x}:${pixel.y}] de la région "${departement.region}" ("${departement.name}") arbore notre couleur !`
+                )
+              ).react(config.reaction.positif);
+            } else {
+              await (
+                await channel.send(
+                  `[${pixel.x}:${pixel.y}] d'un territoire inconnu a adopté la bonne couleur !`
+                )
+              ).react(config.reaction.positif);
+            }
           }
         }
       }
     });
-    await gestionRole({config,image,guild,myDepartement,allrole,channel_log});
+    if(!config.revolution)
+    {
+      await gestionRole({
+        config,
+        image,
+        guild,
+        myDepartement,
+        allrole,
+        channel_log,
+      });
+    }
+
   } catch (e) {
     console.error(e);
     process.exit(1);
