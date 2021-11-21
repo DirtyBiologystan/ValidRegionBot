2fdc4638234aaaeb3cdde7a255dbbd4be8237c5b
FredericBerot-Armand <91632588+FredericBerot-Armand@users.noreply.github.com>
2021-11-21 05:53:37 +0100

    Update image.js

--- src/service/image.js
+++ src/service/image.js
@@ -26,7 +26,8 @@ module.exports = {
       return accu;
     }, []);
     if (pixelToReturn.length) {
-      let pixels = chance.pickset(pixelToReturn, config.numberOfPixelReturn);
+      //let pixels = chance.pickset(pixelToReturn, config.numberOfPixelReturn);
+      let pixels = pixelToReturn.slice(0, config.numberOfPixelReturn);
       return {
         message: `Pour aider, choisissez une des coordonnées ci dessous, et remplacez la couleur par la valeur indiquée (utilisez des réactions pour indiquer quel pixel vous occupez)
 ${
